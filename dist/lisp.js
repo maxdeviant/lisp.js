c2de6b135bd5a097b6557b71c37ee739c88717dc
Marshall Bowers <elliott.codes@gmail.com>
2014-10-03 13:14:00 -0400

    Configure Gulp

---
+++ dist/lisp.js
@@ -0,0 +1,122 @@
+'use strict';
+
+function list () {
+    return Array.prototype.slice.call(arguments);
+}
+
+function car (l) {
+    if (l === null) {
+        return null;
+    }
+
+    return l[0] || null;
+}
+
+function cdr (l) {
+    if (l === null) {
+        return null;
+    }
+
+    var rest = l.slice(1, l.length);
+
+    return rest.length > 0 ? rest : null;
+}
+
+function cddr (l) {
+    if (l === null) {
+        return null;
+    }
+
+    var rest = l.slice(2, l.length);
+
+    return rest.length > 0 ? rest : null;
+}
+
+function cdddr (l) {
+    if (l === null) {
+        return null;
+    }
+
+    var rest = l.slice(3, l.length);
+
+    return rest.length > 0 ? rest : null;
+}
+
+function cddddr (l) {
+    if (l === null) {
+        return null;
+    }
+    
+    var rest = l.slice(4, l.length);
+
+    return rest.length > 0 ? rest : null;
+}
+
+function first (l) {
+    return car(l);
+}
+
+function second (l) {
+    return car(cdr(l));
+}
+
+function third (l) {
+    return car(cddr(l));
+}
+
+function fourth (l) {
+    return car(cdddr(l));
+}
+
+function fifth (l) {
+    return car(cddddr(l));
+}
+
+function sixth (l) {
+    return car(cdr(cddddr(l)));
+}
+
+function seventh (l) {
+    return car(cddr(cddddr(l)));
+}
+
+function eighth (l) {
+    return car(cdddr(cddddr(l)));
+}
+
+function ninth (l) {
+    return car(cddddr(cddddr(l)));
+}
+
+function tenth (l) {
+    return car(cdr(cddddr(cddddr(l))));
+}
+'use strict';
+
+function numberp (s) {
+    return typeof s === 'number';
+}
+
+function zerop (n) {
+    return n === 0;
+}
+
+function minusp (n) {
+    return n < 0;
+}
+
+function plusp (n) {
+    return n > 0;
+}
+
+function evenp (n) {
+    return n % 2 === 0;
+}
+
+function oddp (n) {
+    return Math.abs(n) % 2 === 1;
+}
+
+function listp (s) {
+    return s instanceof Array || s === null;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,32 @@
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('lint', function () {
+    return gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function () {
+    gulp.src('src/*.js')
+        .pipe(concat('lisp.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('lisp.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/*.js', ['lint', 'scripts']);
+});
+
+gulp.task('default', [
+    'lint',
+    'scripts',
+    'watch'
+]);
